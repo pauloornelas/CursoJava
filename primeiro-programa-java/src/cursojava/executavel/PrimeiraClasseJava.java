4112ac049e0a99094a1a3cfcab62723b2628dbe3
Paulo Ornelas <paulo.cardoso@marisapartner.com.br>
2021-09-27 22:30:34 -0300

    Commit teste

--- primeiro-programa-java/src/cursojava/executavel/PrimeiraClasseJava.java
+++ primeiro-programa-java/src/cursojava/executavel/PrimeiraClasseJava.java
@@ -5,9 +5,8 @@ public class PrimeiraClasseJava {
 	/*Main é um método auto executável em Java*/
 	public static void main(String[] args) {
 		
-		/*Escreve texto no console do eclipse no meu note*/
+		/*Escreve texto no console*/
 		System.out.println("Meu primeiro programa em Java");
-		/*Teste de commit*/
 	}
 
 }
