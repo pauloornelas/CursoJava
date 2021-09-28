9f101e40368b72d6540df6edf525bdaafb5b764f
Paulo Ornelas <paulo.cardoso@marisapartner.com.br>
2021-09-28 20:38:44 -0300

    Operadores lógicos ternários - Aula 2 módulo 8

--- primeiro-programa-java/src/cursojava/executavel/PrimeiraClasseJava.java
+++ primeiro-programa-java/src/cursojava/executavel/PrimeiraClasseJava.java
@@ -6,20 +6,25 @@ public class PrimeiraClasseJava {
 	public static void main(String[] args) {
 
 		double nota1 = 90;
-		double nota2 = 90;
-		double nota3 = 90;
+		double nota2 = 10;
+		double nota3 = 30;
 		double nota4 = 20;
 
 		double mediaNota = (nota1 + nota2 + nota3 + nota4) / 4;
 
+		/* Condições lógicas com IF e ELSE */
 		if (mediaNota >= 70) {
 			System.out.println("Aluno aprovado " + mediaNota);
-		} else if (mediaNota > 40) {
+		} else if (mediaNota >= 40) {
 			System.out.println("Aluno de recuperação " + mediaNota);
 		} else {
 			System.out.println("Aluno reprovado " + mediaNota);
 		}
 
+		/* Operações ternárias são para micro validações */
+		String saidaResultado = mediaNota >= 70 ? "Aluno aprovado" : mediaNota >= 40 ? "Aluno de recuperação" : "Aluno reprovado";
+		
+		System.out.println(saidaResultado + " " + mediaNota);
 	}
 
 }
