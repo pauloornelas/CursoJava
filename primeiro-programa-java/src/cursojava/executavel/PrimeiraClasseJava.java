8c6002f379a0e5170e1912a9c96b1be04a3be50b
Paulo Ornelas <paulo.cardoso@marisapartner.com.br>
2021-09-28 09:56:31 -0300

    Aula de tipos de idade - aula 4 módulo 7

--- primeiro-programa-java/src/cursojava/executavel/PrimeiraClasseJava.java
+++ primeiro-programa-java/src/cursojava/executavel/PrimeiraClasseJava.java
@@ -1,12 +1,24 @@
 package cursojava.executavel;
 
 public class PrimeiraClasseJava {
-	
-	/*Main é um método auto executável em Java*/
+
+	/* Main é um método auto executável em Java */
 	public static void main(String[] args) {
-		
-		/*Escreve texto no console*/
+
+		/* Escreve texto no console */
 		System.out.println("Meu primeiro programa em Java");
+
+		int maiorIdade = 18;
+
+		int idadeIdoso = 60;
+
+		int idadeJovem = 16;
+
+		int idadeCrianca = 7;
+
+		String cpf = "469.484.558-45";
+
+		System.out.println("A maior idade é " + maiorIdade);
 	}
 
 }
