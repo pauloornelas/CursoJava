4181b82142744f08e88a9b561d723e3b2c3a5f71
Paulo Ornelas <paulo.cardoso@marisapartner.com.br>
2021-09-28 20:24:37 -0300

    Operações lógicas com IF e Else - Aula 1 módulo 8

--- primeiro-programa-java/src/cursojava/executavel/PrimeiraClasseJava.java
+++ primeiro-programa-java/src/cursojava/executavel/PrimeiraClasseJava.java
@@ -6,14 +6,20 @@ public class PrimeiraClasseJava {
 	public static void main(String[] args) {
 
 		double nota1 = 90;
-		double nota2 = 60;
-		double nota3 = 70;
-		double nota4 = 88;
-		
-		double media = ((nota1 + nota2 + nota3 + nota4)/4);
-		
-		System.out.println("A média das notas é " + media);
-		
+		double nota2 = 90;
+		double nota3 = 90;
+		double nota4 = 20;
+
+		double mediaNota = (nota1 + nota2 + nota3 + nota4) / 4;
+
+		if (mediaNota >= 70) {
+			System.out.println("Aluno aprovado " + mediaNota);
+		} else if (mediaNota > 40) {
+			System.out.println("Aluno de recuperação " + mediaNota);
+		} else {
+			System.out.println("Aluno reprovado " + mediaNota);
+		}
+
 	}
 
 }
