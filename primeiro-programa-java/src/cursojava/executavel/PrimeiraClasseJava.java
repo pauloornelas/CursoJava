7a4b0d6366e8d4a59ac2453ac01bc7a59b475824
Paulo Ornelas <paulo.cardoso@marisapartner.com.br>
2021-09-28 10:09:51 -0300

    Aula sobre variaveis locais e globais - Aula 7 módulo 7

--- primeiro-programa-java/src/cursojava/executavel/PrimeiraClasseJava.java
+++ primeiro-programa-java/src/cursojava/executavel/PrimeiraClasseJava.java
@@ -1,24 +1,22 @@
 package cursojava.executavel;
 
 public class PrimeiraClasseJava {
+	
+	/*Variavel global acessivel a todos os métodos*/
+	 static int maiorIdadeGlobal = 30;
 
 	/* Main é um método auto executável em Java */
 	public static void main(String[] args) {
 
-		/* Escreve texto no console */
-		System.out.println("Meu primeiro programa em Java");
-
+		/*Variavel local porque pertence somente a este método */
 		int maiorIdade = 18;
+		System.out.println("O Valor da variavel local é " + maiorIdade);
+		metodo2();
 
-		int idadeIdoso = 60;
-
-		int idadeJovem = 16;
-
-		int idadeCrianca = 7;
-
-		String cpf = "469.484.558-45";
-
-		System.out.println("A maior idade é " + maiorIdade);
+	}
+	
+	public static void metodo2 () {
+		System.out.println("O valor da variavel Global é " + maiorIdadeGlobal);
 	}
 
 }
