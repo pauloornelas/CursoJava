a529dac95b5c87ab4bafe228c53efdbde3b482b9
Paulo Ornelas <paulo.cardoso@marisapartner.com.br>
2021-09-30 20:24:41 -0300

    Criando métodos para acessar atributos do objeto

--- primeiro-programa-java/src/cursojava/classes/Aluno.java
+++ primeiro-programa-java/src/cursojava/classes/Aluno.java
@@ -1,19 +1,168 @@
 package cursojava.classes;
 
-
 /*Está é a classe/objeto que representará o Aluno*/
 public class Aluno {
+
+	/* Atributos do aluno */
+	private String nome;
+	private int idade;
+	private String dataNascimento;
+	private String rg;
+	private String cpf;
+	private String nomeMae;
+	private String nomePai;
+	private String dataMatricula;
+	private String nomeEscola;
+	private String serieMatriculado;
+
+	private double nota1;
+	private double nota2;
+	private double nota3;
+	private double nota4;
+
+	public Aluno() { /* Cria os dados na memória - sendo padrão do java */
+
+	}
+
+	/* SET é para setar os dados dos atributos */
+	/* GET é para retornar ou pegar os atributos */
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public int getIdade() {
+		return idade;
+	}
+
+	public void setIdade(int idade) {
+		this.idade = idade;
+	}
+
+	public String getDataNascimento() {
+		return dataNascimento;
+	}
+
+	public void setDataNascimento(String dataNascimento) {
+		this.dataNascimento = dataNascimento;
+	}
+
+	public String getRg() {
+		return rg;
+	}
+
+	public void setRg(String rg) {
+		this.rg = rg;
+	}
+
+	public String getCpf() {
+		return cpf;
+	}
+
+	public void setCpf(String cpf) {
+		this.cpf = cpf;
+	}
+
+	public String getNomeMae() {
+		return nomeMae;
+	}
+
+	public void setNomeMae(String nomeMae) {
+		this.nomeMae = nomeMae;
+	}
+
+	public String getNomePai() {
+		return nomePai;
+	}
+
+	public void setNomePai(String nomePai) {
+		this.nomePai = nomePai;
+	}
+
+	public String getDataMatricula() {
+		return dataMatricula;
+	}
+
+	public void setDataMatricula(String dataMatricula) {
+		this.dataMatricula = dataMatricula;
+	}
+
+	public String getNomeEscola() {
+		return nomeEscola;
+	}
+
+	public void setNomeEscola(String nomeEscola) {
+		this.nomeEscola = nomeEscola;
+	}
+
+	public String getSerieMatriculado() {
+		return serieMatriculado;
+	}
+
+	public void setSerieMatriculado(String serieMatriculado) {
+		this.serieMatriculado = serieMatriculado;
+	}
+
+	public double getNota1() {
+		return nota1;
+	}
+
+	public void setNota1(double nota1) {
+		this.nota1 = nota1;
+	}
+
+	public double getNota2() {
+		return nota2;
+	}
+
+	public void setNota2(double nota2) {
+		this.nota2 = nota2;
+	}
+
+	public double getNota3() {
+		return nota3;
+	}
+
+	public void setNota3(double nota3) {
+		this.nota3 = nota3;
+	}
+
+	public double getNota4() {
+		return nota4;
+	}
+
+	public void setNota4(double nota4) {
+		this.nota4 = nota4;
+	}
+
+	public double getMediaNota() {
+		return (nota1 + nota2 + nota3 + nota4) / 4;
+	}
+
+	public boolean getAlunoAprovadoBoolean() {
+		
+		double media = this.getMediaNota();
+		return media >= 70 ? true : false;
+
+	}
 	
-	String nome;
-	int idade;
-	String dataNascimento;
-	String rg;
-	String cpf;
-	String nomeMae;
-	String nomePai;
-	String dataMatricula;
-	String nomeEscola;
-	String serieMatriculado;
-	
+	public String  getAlunoAprovado() {
+		
+		double media = this.getMediaNota();
+		
+		if(media >= 50) {
+			if(media >= 70) {
+				return "aprovado";
+			}else {
+				return "recuperação";
+			}
+		}else {
+			return "reprovado";
+		}
+	}
 
 }
--- primeiro-programa-java/src/cursojava/executavel/PrimeiraClasseJava.java
+++ primeiro-programa-java/src/cursojava/executavel/PrimeiraClasseJava.java
@@ -8,13 +8,36 @@ public class PrimeiraClasseJava {
 
 	/* Main é um método auto executável em Java */
 	public static void main(String[] args) {
-		
-		/*Objeto ainda não existe na memória*/
-		Aluno aluno1;
-		
-		/*Objeto real na memória*/
+
+		/* new Aluno() = Instância ou criação do objeto */
+		/* aluno1 = é uma referência ao objeto aluno */
+		Aluno aluno1 = new Aluno();
 		Aluno aluno2 = new Aluno();
-		
+		Aluno aluno3 = new Aluno();
+		Aluno aluno4 = new Aluno();
+
+		aluno1.setNome(JOptionPane.showInputDialog("Qual o nome do aluno 1?"));
+		aluno1.setIdade((int) Double.parseDouble(JOptionPane.showInputDialog("Qual a idade do aluno 1?")));
+		aluno1.setDataNascimento(JOptionPane.showInputDialog("Qual a data de nascimento do aluno 1?"));
+		aluno1.setRg(JOptionPane.showInputDialog("Qual o RG do aluno 1?"));
+		aluno1.setCpf(JOptionPane.showInputDialog("Qual é o CPF do aluno 1?"));
+		aluno1.setNomeMae(JOptionPane.showInputDialog("Qual o nome da mãe do aluno 1?"));
+		aluno1.setNomePai(JOptionPane.showInputDialog("Qual o nome do pai do aluno 1?"));
+		aluno1.setDataMatricula(JOptionPane.showInputDialog("Qual foi a data da matrícula do aluno 1?"));
+		aluno1.setNomeEscola(JOptionPane.showInputDialog("Qual o nome da escola do aluno 1?"));
+		aluno1.setSerieMatriculado(JOptionPane.showInputDialog("Em qual serie o aluno 1 está matriculado?"));
+
+		aluno1.setNota1(Double.parseDouble(JOptionPane.showInputDialog("Qual a nota 1?")));
+		aluno1.setNota2(Double.parseDouble(JOptionPane.showInputDialog("Qual a nota 2?")));
+		aluno1.setNota3(Double.parseDouble(JOptionPane.showInputDialog("Qual a nota 3?")));
+		aluno1.setNota4(Double.parseDouble(JOptionPane.showInputDialog("Qual a nota 4?")));
+
+		JOptionPane.showMessageDialog(null, "A média do aluno 1 é " + aluno1.getMediaNota());
+
+		System.out.println("O nome do aluno 1 é " + aluno1.getNome());
+		System.out.println("A idade do aluno 1 é " + aluno1.getIdade());
+		System.out.println("O CPF do aluno 1 é " + aluno1.getCpf());
+
 	}
 
 }
