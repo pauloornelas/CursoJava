d1ca8bbfee3f9e74e78ab0d35b9db29c701a9443
Paulo Ornelas <paulo.cardoso@marisapartner.com.br>
2021-09-29 23:52:19 -0300

    Criação de objeto

--- primeiro-programa-java/src/cursojava/classes/Aluno.java
+++ primeiro-programa-java/src/cursojava/classes/Aluno.java
@@ -1,5 +1,19 @@
 package cursojava.classes;
 
+
+/*Está é a classe/objeto que representará o Aluno*/
 public class Aluno {
+	
+	String nome;
+	int idade;
+	String dataNascimento;
+	String rg;
+	String cpf;
+	String nomeMae;
+	String nomePai;
+	String dataMatricula;
+	String nomeEscola;
+	String serieMatriculado;
+	
 
 }
--- primeiro-programa-java/src/cursojava/executavel/PrimeiraClasseJava.java
+++ primeiro-programa-java/src/cursojava/executavel/PrimeiraClasseJava.java
@@ -2,33 +2,19 @@ package cursojava.executavel;
 
 import javax.swing.JOptionPane;
 
+import cursojava.classes.Aluno;
+
 public class PrimeiraClasseJava {
 
 	/* Main é um método auto executável em Java */
 	public static void main(String[] args) {
-
-		String stringNota1 = JOptionPane.showInputDialog(null, "Informe a nota 1.");
-		String stringNota2 = JOptionPane.showInputDialog(null, "Informe a nota 2.");
-		String stringNota3 = JOptionPane.showInputDialog(null, "Informe a nota 3.");
-		String stringNota4 = JOptionPane.showInputDialog(null, "Informe a nota 4.");
-
-		double nota1 = Double.parseDouble(stringNota1);
-		double nota2 = Double.parseDouble(stringNota2);
-		double nota3 = Double.parseDouble(stringNota3);
-		double nota4 = Double.parseDouble(stringNota4);
 		
-		double media = (nota1 + nota2 + nota3 + nota4)/4;
+		/*Objeto ainda não existe na memória*/
+		Aluno aluno1;
 		
-		if(media >= 50) {
-			if(media >= 70) {
-				JOptionPane.showMessageDialog(null, "Aluno aprovado com média " + media);
-			}else {
-				JOptionPane.showMessageDialog(null, "Aluno em recuperação com média " + media);
-			}
-		}else {
-			JOptionPane.showMessageDialog(null, "Aluno reprovado com média " + media);
-		}
-
+		/*Objeto real na memória*/
+		Aluno aluno2 = new Aluno();
+		
 	}
 
 }
