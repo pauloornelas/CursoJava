acfb251f9a8937cdf798e8753d52bba78c295f7a
Paulo Ornelas <paulo.cardoso@marisapartner.com.br>
2022-08-29 22:21:24 -0300

    Primeira aula de Datas

--- primeiro-programa-java/src/modulo_datas/DatasEmJava.java
+++ primeiro-programa-java/src/modulo_datas/DatasEmJava.java
@@ -4,7 +4,15 @@ import java.util.Date;
 
 public class DatasEmJava {
 
-	public static void main() {
+	public static void main(String[] args) {
 		Date data = new Date();
+		
+		System.out.println("Data em milisegundos: " + data.getTime());
+		System.out.println("Dia do mês: " + data.getDate());
+		System.out.println("Dia da semana: " + data.getDay());
+		System.out.println("Horas do dia: " + data.getHours());
+		System.out.println("Minutos da hora: " + data.getMinutes());
+		System.out.println("Segundos dos minutos: " + data.getSeconds());
+		System.out.println("Ano : " + (data.getYear()+1900));
 	}
 }
